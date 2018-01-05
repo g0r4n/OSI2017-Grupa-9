ed75b1eb683e9f9331fbe96b18a10d89d81aed96
BranislavKljaic <34425945+BranislavKljaic@users.noreply.github.com>
2018-01-05 18:28:29 +0100

    comment

--- OSI2017-Grupa-9/OSI2017-Grupa-9/Source.cpp
+++ OSI2017-Grupa-9/OSI2017-Grupa-9/Source.cpp
@@ -3,7 +3,7 @@
 
 void main()
 {
-	::SendMessage(::GetConsoleWindow(), WM_SYSKEYDOWN, VK_RETURN, 0x20000000);
+	::SendMessage(::GetConsoleWindow(), WM_SYSKEYDOWN, VK_RETURN, 0x20000000);	//fullscrean konzola
 	std::fstream file("test.txt", std::ios::in | std::ios::out);
 	runIt();
 	file.close();
